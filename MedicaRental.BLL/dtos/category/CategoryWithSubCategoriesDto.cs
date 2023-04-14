af2aa100ac3c775e745c49aa93a2bb2bd5c55fd7
Khaled Ayman <khaledayman2222@gmail.com>
2023-04-15 00:28:32 +0200

    fix dir (#17)
    Co-authored-by: Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>

--- MedicaRental.BLL/dtos/category/CategoryWithSubCategoriesDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MedicaRental.BLL.Dtos;
-
-public record CategoryWithSubCategoriesDto(int Id, string Name, byte[]? Icon, List<SubCategoryDto> SubCategories);
