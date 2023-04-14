1d115501bf4bb2586b116aa2d85a9e11ef6087c1
Khaled Ayman <khaledayman2222@gmail.com>
2023-04-15 00:43:07 +0200

    After (#19)
    * fix dir 2
    
    * fix dir 3
    
    ---------
    
    Co-authored-by: Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>

---
+++ MedicaRental.BLL/Dto/Validations/UserRoleValidationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using MedicaRental.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicaRental.BLL.Dtos.Validations;
+
+public class UserRoleValidationAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        return Enum.IsDefined(typeof(UserRoles), value!);
+    }
+}
