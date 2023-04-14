2dec7348d9277e286c0bcaed35084002f41a0412
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-14 03:11:12 +0200

    added repo for new model

---
+++ MedicaRental.DAL/Repositories/ItemsPreviousRenters/ItemsPreviousRentersRepo.cs
@@ -0,0 +1,20 @@
+﻿using MedicaRental.DAL.Context;
+using MedicaRental.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicaRental.DAL.Repositories.ItemsPreviousRenters
+{
+    public class ItemsPreviousRentersRepo : EntityRepo<ItemPreviousRenters>, IItemsPreviousRentersRepo
+    {
+        private readonly MedicaRentalDbContext _context;
+
+        public ItemsPreviousRentersRepo(MedicaRentalDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
