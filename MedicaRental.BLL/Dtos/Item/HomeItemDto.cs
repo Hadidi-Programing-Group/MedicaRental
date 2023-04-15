998ce67651fd56752f5422fdadf31b5a8b1291a6
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-15 03:48:10 +0200

    added item DTOs and 2 other DTOs

---
+++ MedicaRental.BLL/Dtos/Item/HomeItemDto.cs
@@ -0,0 +1,32 @@
+﻿using MedicaRental.BLL.Dtos;
+using MedicaRental.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicaRental.BLL.Dtos
+{
+    public record HomeItemDto
+    (
+        string StatusMessage,
+        HttpStatusCode StatusCode,
+        int Id,
+        string Name,
+        string Description,
+        string Serial,
+        string Model,
+        string Make,
+        string Country,
+        int Stock,
+        decimal Price,
+        string Image,
+        bool IsDeleted,
+        int CategoryId,
+        int SubCategoryId,
+        string SellerId,
+        string? CurrentRenterId
+    ) : BaseDtos(StatusMessage, StatusCode);
+}
