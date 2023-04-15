b94bf4cbef56e5139d31c7e2a48689eb11bd8256
Khaled Ayman <khaledayman2222@gmail.com>
2023-04-15 03:45:28 +0200

    Items manager part 1 (#25)
    * implemented ItemsManager part 1
    
    ---------
    
    Co-authored-by: Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>

--- MedicaRental.BLL/Managers/Items/ItemsManager.cs
+++ MedicaRental.BLL/Managers/Items/ItemsManager.cs
@@ -1,11 +1,18 @@
 ﻿using MedicaRental.DAL.UnitOfWork;
-using MedicaRental.BLL.Dtos.Item;
+using MedicaRental.BLL.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using MedicaRental.DAL.Models;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Net;
 
 namespace MedicaRental.BLL.Managers;
 
@@ -18,32 +25,114 @@ public class ItemsManager : IItemsManager
         _unitOfWork = unitOfWork;
     }
 
-    public void AddItemAsync(AddItemDto item)
+    public async Task<StatusDto> AddItemAsync(AddItemDto item)
+    {
+        Item _item = new()
+        {
+            Name = item.Name,
+            Description = item.Description,
+            Serial = item.Serial,
+            Model = item.Model,
+            Make = item.Make,
+            Country = item.Country,
+            Stock = item.Stock,
+            Price = item.Price,
+            Image = Convert.FromBase64String(item.Image),
+            CategoryId = item.CategoryId,
+            SubCategoryId = item.SubCategoryId,
+            SellerId = item.SellerId
+        };
+
+        try
+        {
+            await _unitOfWork.Items.AddAsync(_item);
+            _unitOfWork.Save();
+            return new InsertStatusDto("Item added successfully", HttpStatusCode.Created, _item.Id);
+        }
+        catch (Exception ex)
+        {
+            return new StatusDto (ex.Message, HttpStatusCode.BadRequest);
+        }
+    }
+
+    public async Task<StatusDto> AddItemsAsync(IEnumerable<AddItemDto> items)
+    {
+        List<Item> _items = new();
+        foreach (var item in items)
+        {
+            Item _item = new()
+            {
+                Name = item.Name,
+                Description = item.Description,
+                Serial = item.Serial,
+                Model = item.Model,
+                Make = item.Make,
+                Country = item.Country,
+                Stock = item.Stock,
+                Price = item.Price,
+                Image = Convert.FromBase64String(item.Image),
+                CategoryId = item.CategoryId,
+                SubCategoryId = item.SubCategoryId,
+                SellerId = item.SellerId
+            };
+
+            _items.Add(_item);
+        }
+
+        await _unitOfWork.Items.AddRangeAsync(_items);
+
+        try
+        {
+            await _unitOfWork.Items.AddRangeAsync(_items);
+            _unitOfWork.Save();
+            return new StatusDto("Item added successfully", HttpStatusCode.Created);
+        }
+        catch (Exception ex)
+        {
+            return new StatusDto(ex.Message, HttpStatusCode.BadRequest);
+        }
+    }
+
+    public async Task<StatusDto> DeleteItem(int id)
+    {
+        var succeeded = await _unitOfWork.Items.DeleteOneById(id);
+
+        if(!succeeded) return new StatusDto("No item with the given id.", HttpStatusCode.NotFound);
+
+        try
+        {
+            _unitOfWork.Save();
+            return new StatusDto("Item deleted successfully", HttpStatusCode.NoContent);
+        }
+        catch (Exception ex)
+        {
+            return new StatusDto($"Item couldn't be deleted.\nCause: {ex.Message}", HttpStatusCode.BadRequest);
+        }
+    }
+
+    public async Task<StatusDto> DeleteItems(IEnumerable<int> ids)
+    {
+        var failed = await _unitOfWork.Items.DeleteManyById(ids);
+
+        if (failed.Count > 0) return new StatusDto($"{failed.Count} item ids couldn't be found.\noperation aborted.", HttpStatusCode.NotFound);
+
+        try
+        {
+            _unitOfWork.Save();
+            return new StatusDto("Items deleted successfully", HttpStatusCode.NoContent);
+        }
+        catch (Exception ex)
+        {
+            return new StatusDto($"Items couldn't be deleted.\nCause: {ex.Message}", HttpStatusCode.BadRequest);
+        }
+    }
+
+    public Task<StatusDto> UpdateItem(UpdateItemDto item)
     {
         throw new NotImplementedException();
     }
 
-    public void AddItemsAsync(IEnumerable<AddItemDto> items)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteItem(DeleteItemDto item)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteItems(IEnumerable<DeleteItemDto> items)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteManyItemsById(IEnumerable<int> ids)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void DeleteOneItemById(int id)
+    public Task<StatusDto> UpdateItems()
     {
         throw new NotImplementedException();
     }
@@ -104,16 +193,6 @@ public class ItemsManager : IItemsManager
     }
 
     public Task<IEnumerable<HomeItemDto>> GetItemsBySubCategoryAsync(int subcategoryId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void UpdateItem(UpdateItemDto item)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void UpdateItems()
     {
         throw new NotImplementedException();
     }
