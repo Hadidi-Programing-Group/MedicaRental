c9898b2e47e9eeed1ce68828cf0a32a68f047372
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-16 06:39:29 +0200

    created brands repo

---
+++ MedicaRental.DAL/Repositories/Brand/BrandsRepo.cs
@@ -0,0 +1,20 @@
+﻿using MedicaRental.DAL.Context;
+using MedicaRental.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicaRental.DAL.Repositories
+{
+    public class BrandsRepo : EntityRepo<Brand>, IBrandsRepo
+    {
+        private readonly MedicaRentalDbContext _context;
+
+        public BrandsRepo(MedicaRentalDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
