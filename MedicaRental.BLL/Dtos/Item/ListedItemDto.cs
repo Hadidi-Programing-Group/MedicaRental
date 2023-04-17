d0294ac0d0bf9be3d73f6f180346f1d6a5bad1b1
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-17 20:16:36 +0200

    refactored items dtos to different files

---
+++ MedicaRental.BLL/Dtos/Item/ListedItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicaRental.BLL.Dtos
+{
+    //include categ, subcateg, reviews
+    public record ListedItemDto
+    (
+        Guid Id,
+        string Name,
+        decimal Price,
+        int Stock,
+        string CategoryName,
+        string SubCategoryName,
+        int Rating
+    );
+}
---
+++ MedicaRental.BLL/Dtos/Item/UpdateItemDto.cs
@@ -0,0 +1,21 @@
+﻿using MedicaRental.BLL.Dtos.Validations;
+
+namespace MedicaRental.BLL.Dtos
+{
+    public record UpdateItemDto
+    (
+        Guid Id,
+        string? Name,
+        string? Description,
+        string? Serial,
+        string? Model,
+        int? Stock,
+        decimal? Price,
+        [Base64StringImageValidation]
+        string? Image,
+        bool? IsListed,
+        Guid? BrandId,
+        Guid? CategoryId,
+        Guid? SubCategoryId
+    );
+}
