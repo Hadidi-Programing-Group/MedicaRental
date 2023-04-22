2c815791ac6e8c6043a37779c2fd660cf6da41a2
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-22 08:31:24 +0200

    edited order by function

--- MedicaRental.BLL/Helpers/RentOperationHelper.cs
+++ MedicaRental.BLL/Helpers/RentOperationHelper.cs
@@ -9,6 +9,8 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using MedicaRental.DAL.Repositories;
+using MedicaRental.DAL.Context;
 
 namespace MedicaRental.BLL.Helpers
 {
@@ -58,42 +60,39 @@ namespace MedicaRental.BLL.Helpers
         public static Func<IQueryable<RentOperation>, IIncludableQueryable<RentOperation, object>> RentOperationDtoInclude_Owner = source => source
         .Include(ro => ro.Client).ThenInclude(s => s!.User).Include(ro => ro.Item).Include(ro => ro.Review!);
 
-        public static Func<IQueryable<RentOperation>, IOrderedQueryable<RentOperation>>? GetOrderByQuery(string? orderBy)
+        public static Func<IQueryable<RentOperation>, IOrderedQueryable<RentOperation>>? GetOrderByQuery(string? orderBy, string? searchText)
         {
-            Func<IQueryable<RentOperation>, IOrderedQueryable<RentOperation>>? orderQuery = orderBy switch
+            if (searchText is null)
             {
-                null => null,
-                SharedHelper.HighToLow => new(q => q.OrderByDescending(ro => ro.Price)),
-                SharedHelper.LowToHigh => new(q => q.OrderBy(ro => ro.Price)),
-                SharedHelper.RateDesc => new(q => q.OrderByDescending(ro => ro.Review == null ? 0 : ro.Review.Rating)),
-                SharedHelper.RateAsc => new(q => q.OrderBy(ro => ro.Review == null ? 0 : ro.Review.Rating)),
-                RentDateDesc => new(q => q.OrderByDescending(ro => ro.RentDate)),
-                RentDateAsc => new(q => q.OrderBy(ro => ro.RentDate)),
-                ReturnDateDesc => new(q => q.OrderByDescending(ro => ro.ReturnDate)),
-                ReturnDateAsc => new(q => q.OrderBy(ro => ro.ReturnDate)),
+                return orderBy switch
+                {
+                    null => null,
+                    SharedHelper.HighToLow => new(q => q.OrderByDescending(ro => ro.Price)),
+                    SharedHelper.LowToHigh => new(q => q.OrderBy(ro => ro.Price)),
+                    SharedHelper.RateDesc => new(q => q.OrderByDescending(ro => ro.Review == null ? 0 : ro.Review.Rating)),
+                    SharedHelper.RateAsc => new(q => q.OrderBy(ro => ro.Review == null ? 0 : ro.Review.Rating)),
+                    RentDateDesc => new(q => q.OrderByDescending(ro => ro.RentDate)),
+                    RentDateAsc => new(q => q.OrderBy(ro => ro.RentDate)),
+                    ReturnDateDesc => new(q => q.OrderByDescending(ro => ro.ReturnDate)),
+                    ReturnDateAsc => new(q => q.OrderBy(ro => ro.ReturnDate)),
+                    _ => throw new ArgumentException()
+                };
+            }
+
+            return orderBy switch
+            {
+                null => new(q => q.OrderByDescending(ro => ro.Item)),
+                SharedHelper.HighToLow => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenByDescending(ro => ro.Price)),
+                SharedHelper.LowToHigh => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenBy(ro => ro.Price)),
+                SharedHelper.RateDesc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenByDescending(ro => ro.Review == null ? 0 : ro.Review.Rating)),
+                SharedHelper.RateAsc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenBy(ro => ro.Review == null ? 0 : ro.Review.Rating)),
+                RentDateDesc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenByDescending(ro => ro.RentDate)),
+                RentDateAsc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenBy(ro => ro.RentDate)),
+                ReturnDateDesc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenByDescending(ro => ro.ReturnDate)),
+                ReturnDateAsc => new(q => q.OrderByDescending(ro => MedicaRentalDbContext.LevDist(ro.Item!.Name, searchText, SharedHelper.SearchMaxDistance)).ThenBy(ro => ro.ReturnDate)),
                 _ => throw new ArgumentException()
             };
 
-            return orderQuery;
-        }
-
-        public static Func<IQueryable<RentOperation>, IOrderedQueryable<RentOperation>>? GetOrderByQueryForSearch(string? orderBy, string searchText)
-        {
-            Func<IQueryable<RentOperation>, IOrderedQueryable<RentOperation>>? orderQuery = orderBy switch
-            {
-                null => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText))),
-                SharedHelper.HighToLow => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenByDescending(ro => ro.Price)),
-                SharedHelper.LowToHigh => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenBy(ro => ro.Price)),
-                SharedHelper.RateDesc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenByDescending(ro => ro.Review == null ? 0 : ro.Review.Rating)),
-                SharedHelper.RateAsc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenBy(ro => ro.Review == null ? 0 : ro.Review.Rating)),
-                RentDateDesc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenByDescending(ro => ro.RentDate)),
-                RentDateAsc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenBy(ro => ro.RentDate)),
-                ReturnDateDesc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenByDescending(ro => ro.ReturnDate)),
-                ReturnDateAsc => new(q => q.OrderByDescending(i => SharedHelper.LevDistance(i.Item!.Name, searchText)).ThenBy(ro => ro.ReturnDate)),
-                _ => throw new ArgumentException()
-            };
-
-            return orderQuery;
         }
     }
 }
