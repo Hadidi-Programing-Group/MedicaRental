233b718a0ab70b87dcd6a9326ebd7b8238342e2a
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-23 07:39:46 +0200

    message dtos

---
+++ MedicaRental.BLL/Dtos/Message/ChatDto.cs
@@ -0,0 +1,14 @@
+﻿using MedicaRental.DAL.Models;
+
+namespace MedicaRental.BLL.Dtos
+{
+    public record ChatDto
+    (
+        string UserName,
+        string LastMessage,
+        DateTime MessageDate,
+        MessageStatus MessageStatus,
+        int UnseenMessagesCount,
+        byte[]? UserProfileImage
+    );
+}
