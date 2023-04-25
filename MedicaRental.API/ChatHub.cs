6b54671487bb9346d1865e82a774570b1589f37c
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-25 10:50:03 +0200

    made chat hub save the message on sending and register clients on connection

---
+++ MedicaRental.API/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using MedicaRental.BLL.Managers;
+using MedicaRental.DAL.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicaRental.API
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> _userIds = new Dictionary<string, string>();
+        private readonly IMessagesManager _messagesManager;
+
+        public ChatHub(IMessagesManager messagesManager)
+        {
+            _messagesManager = messagesManager;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            //set all messages sent to him as received and setup notifications
+            if (Context.UserIdentifier is not null)
+            {
+                _userIds.TryAdd(Context.UserIdentifier, Context.ConnectionId);
+            }
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if(Context.UserIdentifier is null) 
+                throw new NullReferenceException(nameof(Context.UserIdentifier));
+
+            if (_userIds.TryGetValue(Context.UserIdentifier, out var user))
+            {
+                _userIds.Remove(Context.UserIdentifier);
+            }
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+        public async Task SendMessage(string message, string receiverId, DateTime timeStamp)
+        {
+            if (_userIds.TryGetValue(receiverId, out string? conId))
+            {
+                await Clients.Client(conId).SendAsync("ReceiveMessage", message, Context.UserIdentifier);
+                await _messagesManager.AddMessage(Context.UserIdentifier!, receiverId, message, timeStamp);
+            }
+        }
+    }
+}
