67029a8ed9c95d0a4d772f9ebef97a5aef1bceb2
Khaled Ayman <49253072+KhaledAyman2222@users.noreply.github.com>
2023-04-25 10:48:47 +0200

    fixed some logic

--- MedicaRental.API/Controllers/MessagesController.cs
+++ MedicaRental.API/Controllers/MessagesController.cs
@@ -1,6 +1,8 @@
 ﻿using MedicaRental.BLL.Dtos;
 using MedicaRental.BLL.Managers;
+using MedicaRental.DAL.Context;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicaRental.API.Controllers
@@ -10,22 +12,28 @@ namespace MedicaRental.API.Controllers
     public class MessagesController : ControllerBase
     {
         private readonly IMessagesManager _messagesManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public MessagesController(IMessagesManager messagesManager)
+        public MessagesController(IMessagesManager messagesManager, UserManager<AppUser> userManager)
         {
             _messagesManager = messagesManager;
+            _userManager = userManager;
         }
 
         [HttpGet("allChats")]
-        public async Task<ActionResult<IEnumerable<ChatDto>>> GetUserChats(string userId, int upTo)
+        public async Task<ActionResult<IEnumerable<ChatDto>>> GetUserChats(int upTo)
         {
-            return Ok(await _messagesManager.GetUserChats(userId, upTo));
+            var userId = _userManager.GetUserId(User);
+            var chats = await _messagesManager.GetUserChats(userId, upTo);
+            return Ok(chats);
         }
 
         [HttpGet("chat")]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetChat(string firstUserId, string secondUserId, int upTo, DateTime dateOpened)
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetChat(string secondUserId, int upTo, DateTime dateOpened)
         {
-            return Ok(await _messagesManager.GetChat(firstUserId, secondUserId, upTo, dateOpened));
+            var userId = _userManager.GetUserId(User);
+            var chat = await _messagesManager.GetChat(userId, secondUserId, upTo, dateOpened);
+            return Ok(chat);
         }
 
         [HttpGet("delete")]
