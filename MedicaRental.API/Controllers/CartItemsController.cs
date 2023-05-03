11c13747e42b8d56e04d21ed95fede712cdaf49a
Mina Yossry <mina.yossry4@gmail.com>
2023-05-03 12:18:00 +0300

    add cart items controller

---
+++ MedicaRental.API/Controllers/CartItemsController.cs
@@ -0,0 +1,19 @@
+﻿using MedicaRental.BLL.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicaRental.API.Controllers;
+
+public class CartItemsController : Controller
+{
+    private readonly ICartItemsManager _cartItemsManager;
+
+    public CartItemsController(ICartItemsManager cartItemsManager)
+    {
+        _cartItemsManager = cartItemsManager;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
