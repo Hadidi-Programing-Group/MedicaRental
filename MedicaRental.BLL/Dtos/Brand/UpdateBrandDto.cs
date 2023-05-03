22d82461ef3c39e901782a6536816531949c9081
HebaTullah Abdelazem <57576774+Hebaallah61@users.noreply.github.com>
2023-05-04 01:19:07 +0300

    Brands Endpoints

---
+++ MedicaRental.BLL/Dtos/Brand/UpdateBrandDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicaRental.BLL.Dtos.Brand;
+
+public record UpdateBrandDto( string Name, string CountryOfOrigin, string Image);
--- MedicaRental.BLL/Managers/Brands/IBrandsManager.cs
+++ MedicaRental.BLL/Managers/Brands/IBrandsManager.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using MedicaRental.BLL.Dtos;
+using MedicaRental.BLL.Dtos.Brand;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MedicaRental.BLL.Managers
+namespace MedicaRental.BLL.Managers;
+
+public interface IBrandsManager
 {
-    public interface IBrandsManager
-    {
-    }
+    Task<DeleteBrandStatusDto> DeleteByIdAsync(Guid id);
+    Task<IEnumerable<BrandDto>> GetAllAsyc();
+    Task<BrandDto> GetBrandbyId(Guid id);
+    Task<InsertBrandStatusDto> InsertNewBrand(InsertBrandDto insertBrandDto);
+    Task<UpdateBrandStatusDto> UpdateNewBrand(Guid id, UpdateBrandDto updateBrandDto);
 }
