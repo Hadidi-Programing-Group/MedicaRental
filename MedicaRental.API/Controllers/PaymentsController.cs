d2343dbb2374cc79b1ff10921f600000240f34cf
عبدالله الحديدي <abdallah201@protonmail.com>
2023-05-03 12:53:05 +0300

    webhock Added

--- MedicaRental.API/Controllers/PaymentsController.cs
+++ MedicaRental.API/Controllers/PaymentsController.cs
@@ -31,13 +31,56 @@ namespace MedicaRental.API.Controllers
                 {
                     Enabled = true,
                 },
+
             });
             Console.WriteLine($"Client Sercre {paymentIntent.ClientSecret}");
             //return Ok(new { client_secret = paymentIntent.ClientSecret });
             return Ok(paymentIntent.ToJson());
 
         }
+        [HttpPost("webhook")]
+        public async Task<IActionResult> Index()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            const string endpointSecret = "whsec_26839dec5f0f476ef9b48676d3fb0fbff6953e35133eb5e20b6eab9d9e310e54";
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json);
+                var signatureHeader = Request.Headers["Stripe-Signature"];
 
+                stripeEvent = EventUtility.ConstructEvent(json,
+                        signatureHeader, endpointSecret);
+
+                if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    Console.WriteLine("A successful payment for {0} was made.", paymentIntent.Amount);
+                    // Then define and call a method to handle the successful payment intent.
+                    // handlePaymentIntentSucceeded(paymentIntent);
+                }
+                else if (stripeEvent.Type == Events.PaymentMethodAttached)
+                {
+                    var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
+                    // Then define and call a method to handle the successful attachment of a PaymentMethod.
+                    // handlePaymentMethodAttached(paymentMethod);
+                }
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
     }
 
 }
+
